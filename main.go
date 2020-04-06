1dcdfc78a4c102f1e7af00e6702bf711b480b00a
Ildar Valiullin <preved.911@gmail.com>
2020-04-06 23:49:46 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// override restic executable path
+	resticExecPath := os.Getenv("RESTIC_BINARY_PATH")
+	if resticExecPath == "" {
+		resticExecPath = "/usr/local/bin/restic"
+	}
+
+	godotenv.Load("/etc/default/restic")
+
+	logFile, err := os.OpenFile("/var/log/restic/restic.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("log file open error: %\ns", err)
+	}
+	defer logFile.Close()
+
+	if os.Args[1] == "backup" {
+		log.SetOutput(io.MultiWriter(os.Stdout, logFile))
+	}
+
+	os.Setenv(
+		"RESTIC_REPOSITORY",
+		fmt.Sprintf("%s/%s",
+			os.Getenv("RESTIC_REPOSITORY_BUCKET"),
+			os.Getenv("RESTIC_REPOSITORY_PREFIX")))
+
+	cmd := exec.Command(resticExecPath, os.Args[1:]...)
+
+	var stdout []byte
+	var stderr error
+
+	// 5 attempts before failed state return
+	for i := 0; i < 5; i++ {
+		stdout, stderr = cmd.CombinedOutput()
+		if stderr != nil {
+			log.Printf("check failed: %s\n", stderr)
+			time.Sleep(30 * time.Second)
+		} else {
+			break
+		}
+	}
+
+	log.Println(string(stdout))
+
+	if stderr != nil {
+		log.Fatalf("%s", stderr)
+	}
+}
